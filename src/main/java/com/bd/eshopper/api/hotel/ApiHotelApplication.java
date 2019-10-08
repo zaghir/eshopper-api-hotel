ec8b8a295b86c6b7ff42b546df486ce63b5f440b
zaghir <zaghir.youssef@gmail.com>
2019-10-08 14:19:13 +0200

    first commit

---
+++ src/main/java/com/bd/eshopper/api/hotel/ApiHotelApplication.java
@@ -0,0 +1,13 @@
+package com.bd.eshopper.api.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bd/eshopper/api/hotel/ApiHotelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bd.eshopper.api.hotel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiHotelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
